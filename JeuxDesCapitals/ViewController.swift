fe251d35b23bc54866bbb5f2e136981e5408f2ea
Mathias Erligmann <mathiaserligmann@mbp-de-mathias>
2018-09-24 02:33:38 +0200

    Initial Commit

---
+++ JeuxDesCapitals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JeuxDesCapitals
+//
+//  Created by Mathias Erligmann on 24/09/2018.
+//  Copyright © 2018 Mathias Erligmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
